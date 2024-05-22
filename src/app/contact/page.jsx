5e4f404f5729a4cdd31de0fbc9a5d06047c0d32b
Alberto Mrtz <alberto@intelligential.tech>
2024-05-22 13:50:50 -0600

    [emailJS] - implement email library

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
 
 const ContactPage = () => {
 
@@ -9,6 +10,29 @@ const ContactPage = () => {
   const [error, setError] = useState(false);
 
   const text = "Say Hello!";
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setError(false);
+    setSuccess(true);
+
+    emailjs
+      .sendForm(process.env.NEXT_PUBLIC_SERVICE_KEY, process.env.NEXT_PUBLIC_TEMPLATE_KEY, form.current, {
+        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          setSuccess(true);
+          form.current.reset();
+        },
+        (error) => {
+          setError(true);
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
 
   return (
     <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration: 1}}>
@@ -31,13 +55,14 @@ const ContactPage = () => {
             😀
           </div>
         </div>
-        <form className="h-1/2 lg:h-full lg:w-1/2 bd-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24">
-          <span>Dear Alberto Martinez</span>
-          <textarea name="user_message" id="msg" rows={6} className="bg-transparent border-b-2 border-b-black outline-none resize-none"></textarea>
+        <form ref={form} onSubmit={sendEmail} className="h-1/2 lg:h-full lg:w-1/2 bd-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24">
+          <input name="user_name" type="text" placeholder="write your name here!" className="bg-transparent border-b-2 border-b-black outline-none" />
+          <span>Dear Alberto Martinez, </span>
+          <textarea name="user_message" id="msg" rows={6} placeholder="write your message" className="bg-transparent border-b-2 border-b-black outline-none resize-none"></textarea>
           <span>My email address is:</span>
-          <input name="user_email" type="email" className="bg-transparent border-b-2 border-b-black outline-none"/>
+          <input name="user_email" type="email" placeholder="write your contact e-mail" className="bg-transparent border-b-2 border-b-black outline-none"/>
           <span>Regards</span>
-          <button className="bg-purple-200 rounded font-semibold tet-gray-600 p-4">Send</button>
+          <button type="submit" className="bg-purple-200 rounded font-semibold tet-gray-600 p-4">Send</button>
           {success && <span className="text-green-600 font-semibold">Your message has been sent successfully!</span>}
           {error && <span className="text-red-600 font-semibold">Something went wrong! <br/> You should send a private message to jalberto.martinez.fs@gmail.com</span>}
         </form>
